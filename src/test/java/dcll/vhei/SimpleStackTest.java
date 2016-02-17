73972a78abbc597a63cc23efc3a46116950afadb
BlueSkyBalmung <vincent.heit@gmail.com>
2016-02-17 14:23:53 +0100

    Ajout Annotation @Test

--- src/test/java/dcll/vhei/SimpleStackTest.java
+++ src/test/java/dcll/vhei/SimpleStackTest.java
@@ -1,39 +1,41 @@
 package dcll.vhei;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 import java.util.EmptyStackException;
 
 public class SimpleStackTest extends TestCase {
+    @Test
     public void testIsEmpty(){
         SimpleStack st=new SimpleStackImpl();
         assertEquals(true,st.isEmpty());
     }
-
+    @Test
     public void testIsNotEmpty(){
         SimpleStack st=new SimpleStackImpl();
         st.push(new Item());
         assertEquals(false, st.isEmpty());
     }
-
+    @Test
     public void testSizeEmpty(){
        SimpleStack st = new SimpleStackImpl();
         assertEquals(0,st.getSize());
     }
-
+    @Test
     public void testSizeNotEmpty(){
         SimpleStack st = new SimpleStackImpl();
         st.push(new Item());
         assertEquals(1, st.getSize());
     }
-
+    @Test
     public void testPush(){
         SimpleStack st = new SimpleStackImpl();
         Item i=new Item();
         st.push(i);
         assertEquals(false,st.isEmpty());
     }
-
+    @Test
     public void testPeek(){
         SimpleStack st = new SimpleStackImpl();
         Item i=new Item();
@@ -41,7 +43,7 @@ public class SimpleStackTest extends TestCase {
         assertEquals(i,st.peek());
         assertEquals(false,st.isEmpty());
     }
-
+    @Test
     public void testPop(){
         SimpleStack st = new SimpleStackImpl();
         Item i=new Item();
@@ -49,7 +51,7 @@ public class SimpleStackTest extends TestCase {
         assertEquals(i, st.pop());
         assertEquals(true,st.isEmpty());
     }
-
+    @Test
     public void testEmptyStackExceptionPop(){
         boolean test=false;
         try{
@@ -61,7 +63,7 @@ public class SimpleStackTest extends TestCase {
         }
         assertEquals(true,test);
     }
-
+    @Test
     public void testEmptyStackExceptionPeek(){
         boolean test=false;
         try{
