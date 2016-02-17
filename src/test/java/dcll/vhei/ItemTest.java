254c697642eff0bd274c4b141a8ed8ac0549875a
BlueSkyBalmung <vincent.heit@gmail.com>
2016-02-17 15:05:00 +0100

    Modification test

---
+++ src/test/java/dcll/vhei/ItemTest.java
@@ -0,0 +1,7 @@
+package dcll.vhei;
+
+import static org.junit.Assert.*;
+
+public class ItemTest {
+
+}
--- src/test/java/dcll/vhei/SimpleStackTest.java
+++ src/test/java/dcll/vhei/SimpleStackTest.java
@@ -1,78 +1,71 @@
 package dcll.vhei;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 
-import java.util.EmptyStackException;
+import static org.junit.Assert.*;
 
-public class SimpleStackTest extends TestCase {
+import java.util.EmptyStackException;
+/*
+    répond à l'exigence #1
+ */
+public class SimpleStackTest {
     @Test
     public void testIsEmpty(){
         SimpleStack st=new SimpleStackImpl();
-        assertEquals(true,st.isEmpty());
+        assertEquals("isEmpty marche",true,st.isEmpty());
     }
     @Test
     public void testIsNotEmpty(){
         SimpleStack st=new SimpleStackImpl();
         st.push(new Item());
-        assertEquals(false, st.isEmpty());
+        assertEquals("IsNotEmpty marche",false, st.isEmpty());
     }
     @Test
     public void testSizeEmpty(){
        SimpleStack st = new SimpleStackImpl();
-        assertEquals(0,st.getSize());
+        assertEquals("sizeEmpty marche",0,st.getSize());
     }
     @Test
     public void testSizeNotEmpty(){
         SimpleStack st = new SimpleStackImpl();
         st.push(new Item());
-        assertEquals(1, st.getSize());
+        assertEquals("SizeNotEmpty marche",1, st.getSize());
     }
     @Test
     public void testPush(){
         SimpleStack st = new SimpleStackImpl();
         Item i=new Item();
         st.push(i);
-        assertEquals(false,st.isEmpty());
+        assertEquals("testPush la pile n'est plus vide",false,st.isEmpty());
     }
     @Test
     public void testPeek(){
         SimpleStack st = new SimpleStackImpl();
         Item i=new Item();
         st.push(i);
-        assertEquals(i,st.peek());
-        assertEquals(false,st.isEmpty());
+        assertEquals("La pile renvoie bien le bon item", i, st.peek());
+        assertEquals("la pile ne s'est pas vidé",false,st.isEmpty());
     }
     @Test
     public void testPop(){
         SimpleStack st = new SimpleStackImpl();
         Item i=new Item();
+        st.push(new Item());
         st.push(i);
-        assertEquals(i, st.pop());
-        assertEquals(true,st.isEmpty());
+        assertEquals("La pile renvoie bien le bon item", i, st.pop());
+        assertEquals("La pile redevient vide",1,st.getSize());
     }
-    @Test
-    public void testEmptyStackExceptionPop(){
-        boolean test=false;
-        try{
+    @Test(expected = EmptyStackException.class)
+    public void testEmptyStackExceptionPop() throws EmptyStackException{
+
             SimpleStack st = new SimpleStackImpl();
             st.pop();
-        }catch(EmptyStackException e){
-            test=true;
-            assertTrue(e instanceof EmptyStackException);
-        }
-        assertEquals(true,test);
     }
-    @Test
-    public void testEmptyStackExceptionPeek(){
-        boolean test=false;
-        try{
+    @Test(expected = EmptyStackException.class)
+    public void testEmptyStackExceptionPeek() throws EmptyStackException{
+
             SimpleStack st = new SimpleStackImpl();
             st.peek();
-        }catch(EmptyStackException e){
-            test=true;
-            assertTrue(e instanceof EmptyStackException);
-        }
-        assertEquals(true,test);
+
     }
 }
